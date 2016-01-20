8c065266a7cfb557d452a8e95c51dcff3effca75
mrz <default.mail@mail.ru>
2016-01-20 14:54:50 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HotelAdvisor::Application.config.secret_key_base = 'd1cddae5d6c73fd8a7ca1f2e3cad336ee8b4d6d85b7e6f3f0e85dd9be3f45903951ec39c1f9a7d547c516753ee28ef431e276b5a94f187c819dcabfa3b620d68'
